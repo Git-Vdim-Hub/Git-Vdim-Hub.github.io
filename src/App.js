d796ff6102d2b41a42dc9815b86020e2cea53c9d
Git-Vdim-Hub <vadimrace@gmail.com>
2023-03-03 17:26:29 -0500

    added Navbar shell code

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Header from './components/Header';
 import Footer from './components/Footer';
-import Navigation from './components/Navigation';
 import Project from './components/Project'
 // This function is a functional component that helps us split the UI into distinct parts.
 // In this case, we are returning four components, <Header/>, <Navigation/>, <Project/>, <Footer/> from it.
@@ -9,7 +8,6 @@ function App() {
   return(
     <body>
         <Header/>
-        <Navigation/>
         <Project/>
         <Footer/>
     </body>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import Navigation from './Navigation'
 // Components are responsible for rendering some part of an application's UI.
 // Components let us split the UI into independent, reusable pieces, and also think about each piece in isolation.
 function Header() {
-  const text = 'some text';
+  const text = 'Pikachu Text';
   // JSX is a syntax extension to JavaScript that looks like HTML.
   // JSX is compiled by "Babel" into widely supported ES5 syntax.
-  return <p>Header {text}</p>;
+  return (<header>Header {text}
+    <Navigation/>
+    </header>);
 }
 
 export default Header;
