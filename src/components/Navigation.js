08eac0a806086039fe55ad5fb626c2f6bbf159de
Git-Vdim-Hub <vadimrace@gmail.com>
2023-03-03 21:09:57 -0500

    working Conditional Rendering for pages

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,19 +1,25 @@
 import React from 'react';
 
-export default function Navigation(){
+export default function Navigation({currentPage, handlePageChange}){
     return(
 <nav className="navbar navbar-expand-lg bg-body-tertiary">
   <div className="container-fluid">
     <a className="navbar-brand" href="#">Navbar</a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
+    <span className="navbar-toggler-icon"></span>
     </button>
     <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
       <div className="navbar-nav">
-        <a className="nav-link active" aria-current="page" href="#">Home</a>
-        <a className="nav-link" href="#">Features</a>
-        <a className="nav-link" href="#">Pricing</a>
-        <a className="nav-link disabled">Disabled</a>
+        <a href="#about"
+           onClick={() => handlePageChange('About')}
+           
+           className={currentPage === 'About' ? 'nav-link active' : 'nav-link'}
+           >
+            About
+          </a>
+        <a className="nav-link" href="#portfolio">Portfolio</a>
+        <a className="nav-link" href="#resume">Resume</a>
+        <a className="nav-link" href="#contact">Contact</a>
       </div>
     </div>
   </div>
--- src/components/Project.js
+++ src/components/Project.js
@@ -1,7 +1,35 @@
-import React from 'react';
+import React, {useState} from 'react';
+import Navigation from './Navigation';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Portfolio from './pages/Portfolio';
+import Resume from './pages/Resume';
 
 export default function Project(){
+    const [currentPage, setCurrentPage] = useState('About');
+      // This method is checking to see what the value of `currentPage` is. Depending on the value of currentPage, we return the corresponding component to render.
+      const renderPage = () => {
+        if(currentPage === 'About') {
+            return <About />;
+        }
+        if(currentPage === 'Portfolio') {
+            return <Portfolio />;
+        }
+        if(currentPage === 'Resume') {
+            return <Resume />;
+        }
+        if(currentPage === 'Contact') {
+            return <Contact />;
+        }
+        
+      };
+
+      const handlePageChange = (page) => setCurrentPage(page);
     return(
-        <section>Project</section>
+        <div>
+            <Navigation currentPage = {currentPage}
+            handlePageChange={handlePageChange} />
+            {renderPage()}
+        </div>
     )
 }
