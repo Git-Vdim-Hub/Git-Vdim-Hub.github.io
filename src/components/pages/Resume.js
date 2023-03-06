681f210df09826e7f4a8eee4b61f5bb88763468c
Git-Vdim-Hub <vadimrace@gmail.com>
2023-03-06 17:07:56 -0500

    Resume page built

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -1,10 +1,46 @@
 import React from 'react';
 
+
 export default function Resume() {
     return (
         <div>
-            <a href="#">Download My Resume</a>
-            <a>Front End</a>
+            <div className = "container p-2 ">
+            <ul class="list-unstyled">
+                <li>Front-End Proficiencies
+                    <ul>
+                    <li>HTML</li>
+                    <li>CSS</li>
+                    <li>JavaScript</li>
+                    <li>jQuery</li>
+                    <li>responsive design</li>
+                    <li>React</li>
+                    <li>Bootstrap</li>
+                    </ul>
+                </li>
+                </ul>
+                <ul class="list-unstyled">
+                <li>Back-End Proficiencies
+                    <ul>
+                    <li>APIs</li>
+                    <li>Node</li>
+                    <li>Express</li>
+                    <li>MySQL, Sequelize</li>
+                    <li>MongoDB, Mongoose</li>
+                    <li>REST</li>
+                    <li>GraphQL</li>
+                    </ul>
+                </li>
+                </ul>
+            </div>
+            <div className="container">
+                <div className="row">
+                    <div className = "col-md-6 offset-md-5">
+                    <a href="https://github.com/Git-Vdim-Hub/Resume" className="text-decoration-none">Download My Resume</a>
+                    </div>
+                </div>
+            </div>
         </div>
+            
+
     )
 }
