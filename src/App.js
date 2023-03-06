3acb64aab927748d39fc1b720a387713f6014f31
Git-Vdim-Hub <vadimrace@gmail.com>
2023-03-06 10:03:30 -0500

    fixed sizing issue

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@
 import React, {useState} from 'react';
 import Header from './components/Header';
 import Footer from './components/Footer';
-import Project from './components/Project'
+import Project from './components/Project';
+import './style.css';
 // This function is a functional component that helps us split the UI into distinct parts.
 // In this case, we are returning four components, <Header/>, <Navigation/>, <Project/>, <Footer/> from it.
 function App() {
--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -2,12 +2,12 @@ import React from 'react';
 
 export default function About () {
     return (
-        <div className="container " style={{ display: "block", width: 10000, padding: 20 }}>
+        <div className="container" style={{ display: "block", width: "100%", padding: 20 }}>
             <h4> About Me</h4>
             <img src='/images/Vadim1.png' className='img-fluid rounded' />
             <p className="mt-2">Hello and welcome to my React Portfolio!</p>
-            <p>I am a Developmental Engineer for the United States Air Force (USAF). My specific engineering discipline is Electrical Engineering, but the majority of the time I use this knowledge to carry out decisions as a program manager/team leader. Besides electronics, hardware and program management I love to code when ever my job permits and also on my free time in the defi/crypto world.</p>
-            <p> The majority of my professional career has been in the USAF. I have attended many leadership schools including Basic Military Training, Airman Leadership School, Non-Comissioned Officer Academy and Officer Training School. During my career in the Air Force I have led many teams in technical and non-technical roles. Most recently I have completed a tour leading a team of eight signal analysts and engineers analyzing radar signals.</p>
+            <p className = " mt-2 overflow-auto">I am a Developmental Engineer for the United States Air Force (USAF). My specific engineering discipline is Electrical Engineering, but the majority of the time I use this knowledge to carry out decisions as a program manager/team leader. Besides electronics, hardware and program management I love to code when ever my job permits and also on my free time in the defi/crypto world.</p>
+            <p className = " mt-2 overflow-auto"> The majority of my professional career has been in the USAF. I have attended many leadership schools including Basic Military Training, Airman Leadership School, Non-Comissioned Officer Academy and Officer Training School. During my career in the Air Force I have led many teams in technical and non-technical roles. Most recently I have completed a tour leading a team of eight signal analysts and engineers analyzing radar signals.</p>
         </div>
 
     )
